bb540db3df1f07b459282ef302f582af0fa6342c
Fubero <50967553+Fubero@users.noreply.github.com>
2024-10-27 22:17:14 +0100

    logo and first input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::fs;
+
 fn main() {
-    println!("Wellcome to TurnOrder");
+    let logo = fs::read_to_string("./assets/logo.txt").expect("Can't open the impressiv logo...");
+    println!("{logo}");
+
+    loop {
+        println!("What is your command, Master!");
+        
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).expect("Failed to read input!");
+        command = command.trim().to_string();
+
+        println!("Your command was {command}");
+
+        if command == "end"{
+            break;
+        }
+    }
 }
